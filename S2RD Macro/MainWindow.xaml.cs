a96d02936dcfcd5748721b9393e82770fe5bea35
ighook <jinbeom4292@gmail.com>
2024-02-21 13:45:37 +0900

    first commit

---
+++ S2RD Macro/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace S2RD_Macro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        public static extern int PostMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string SClassName, string SWindowName);
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr hWnd1, IntPtr hWnd2, string lpsz1, string lpsz2);
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr findname);
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr findname, int howShow);
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
